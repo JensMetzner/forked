05fed78de58b22ec238e1b7e83cdc3b644b29d09
Jens Metzner <jens.metzner@uni-konstanz.de>
2020-11-22 22:51:37 +0100

    :bug: clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -267,7 +267,7 @@ async fn run(opts: Opts) -> Result<(), anyhow::Error> {
 
                         let title = lines
                             .next()
-                            .unwrap_or_else(|| "Feedback")
+                            .unwrap_or("Feedback")
                             .trim_matches('#')
                             .trim()
                             .to_string();
